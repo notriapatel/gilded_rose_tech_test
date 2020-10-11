10d1c5014a452412e6815a00376e973eb7966dbd
ria patel <ria.amy.patel@gmail.com>
2020-10-11 12:45:10 +0100

    add test for regular items before sellIn quality decreases by 1

--- spec/gildedRoseSpec.js
+++ spec/gildedRoseSpec.js
@@ -6,4 +6,12 @@ describe("Gilded Rose", function() {
     expect(items[0].name).toEqual("foo");
   });
 
+  describe("regular items", function() {
+    it("quality decreases by 1 before sellIn", function() {
+      const gildedRose = new Shop([ new Item("regularItem", 5, 5) ]);
+      const items = gildedRose.updateQuality();
+      expect(items[0].sellIn).toEqual(4);
+      expect(items[0].quality).toEqual(4)
+    });
+  });
 });
